8528d2d9d8147741a33edeb6153807db36ac08ac
chicloon <chicloon@gmail.com>
2018-11-27 12:18:12 +0900

    client initial setup

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "/api"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getTodos {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
